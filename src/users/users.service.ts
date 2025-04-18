7cd10e8f03a03db4f967ee7d0051bddae4b614c2
missiona.carla <missiona.carla@gmail.com>
2025-04-18 08:10:22 +0000

    erroer

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,7 +8,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User, Profile} from '../entities/user';
+import { User} from '../entities/user';
 
 @Injectable()
 export class UsersService {
